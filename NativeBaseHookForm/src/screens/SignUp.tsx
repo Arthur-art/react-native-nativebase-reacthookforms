19f623a324996f650085729d4c0660f93389cb2e
arthur-art <arthurteixeira.guts@gmail.com>
2023-12-01 20:08:22 -0300

    Iniciando e configurando o projeto, trabalhando com nativebase

---
+++ NativeBaseHookForm/src/screens/SignUp.tsx
@@ -0,0 +1,9 @@
+import { VStack, Heading } from 'native-base'
+
+export default function SignUp() {
+  return (
+    <VStack bgColor={'gray.500'} flex={1}>
+      <Heading my={24}>Olá Arthur!</Heading>
+    </VStack>
+  );
+}

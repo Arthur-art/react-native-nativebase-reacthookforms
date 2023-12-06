23ce7ea693c2895a6105030495548d7a21066852
Arthur-art <arthurteixeira.guts@gmail.com>
2023-12-05 22:15:36 -0300

    Trabalhando com react-hook-form no react native

--- NativeBaseHookForm/src/screens/SignUp.tsx
+++ NativeBaseHookForm/src/screens/SignUp.tsx
@@ -1,16 +1,45 @@
 import { VStack, Heading, Center } from 'native-base'
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
+import { useForm, Controller } from 'react-hook-form';
 
 export function SignUp() {
+  const { control } = useForm();
   return (
     <VStack bgColor={'gray.300'} flex={1} px={10}>
       <Center>
         <Heading my={24}>Crie a sua conta!</Heading>
-        <Input isInvalid errorMessage="Informe o nome." placeholder="Nome" />
-        <Input placeholder="E-mail" errorMessage="Informe o E-mail." />
-        <Input placeholder="Senha" errorMessage="Informe a Senha." />
-        <Input placeholder="Confirme a senha" />
+        <Controller
+          control={control}
+          name='Nome'
+          render={({ field: { onChange } }) => (
+            <Input placeholder="Nome" onChangeText={onChange} />
+          )}
+        />
+
+        <Controller
+          name='E-mail'
+          control={control}
+          render={({ field: { onChange } }) => (
+            <Input placeholder="E-mail" onChangeText={onChange} />
+          )}
+        />
+
+        <Controller
+          name='Senha'
+          control={control}
+          render={({ field: { onChange } }) => (
+            <Input placeholder="Senha" onChange={onChange} secureTextEntry />
+          )}
+        />
+
+        <Controller
+          name='Confirmar-Senha'
+          control={control}
+          render={({ field: { onChange } }) => (
+            <Input placeholder="Confirme a senha" onChange={onChange} secureTextEntry/>
+          )}
+        />
         <Button title="Cadastrar" />
       </Center>
     </VStack>
